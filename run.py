7d948f831d8a87cb5b03933572917bb0389834f4
Amnasaqib <>
2024-01-07 23:48:08 +0000

    Import libraries

--- run.py
+++ run.py
@@ -1,3 +1,9 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+"""
+Main file to run the application.
+"""
+
+# Import libraries/packages.
+import os
+import datetime
+import gspread
+from google.oauth2.service_account import Credentials
